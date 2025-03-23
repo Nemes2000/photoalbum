c0023bde10d77ea9cd494b05c57c82dabe01f248
=Nemes2000 <=nemes.attila.02.02@gmail.com>
2025-03-23 11:53:41 +0100

    add docker and postgresql

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.12
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+ENV PYTHONUNBUFFERED=1
+RUN python manage.py collectstatic --noinput
+
+EXPOSE 8000
+
+# Start Gunicorn server
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "photoalbum.wsgi"]
--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'photoalbum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photodb',
+        'USER': 'Admin',
+        'PASSWORD': 'admin',
+        'HOST': 'postgresql',
+        'PORT': '5432',
     }
 }
 

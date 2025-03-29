e0fc4ad4353491bbc80d3f5bab85834d114a9d37
=Nemes2000 <=nemes.attila.02.02@gmail.com>
2025-03-29 09:40:59 +0100

    t

--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -139,7 +139,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
